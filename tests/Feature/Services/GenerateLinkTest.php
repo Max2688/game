a9fdcc7a0b2ee6ba1abf9388222b9b205a16cf59
Max2688 <jumper2388@gmail.com>
2024-08-04 12:55:40 +0200

    add testing service generateLink

---
+++ tests/Feature/Services/GenerateLinkTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Services;
+
+use App\Models\User;
+use App\Services\GenerateLink;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GenerateLinkTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGenerateLink()
+    {
+        $user = User::factory()->create();
+
+        $generateLinkService = new GenerateLink();
+
+        $this->actingAs($user);
+
+        $uniqueLink = $generateLinkService->generateLink($user);
+
+        $this->assertDatabaseHas('links', [
+            'user_id' => $user->id,
+            'unique_link' => $uniqueLink,
+        ]);
+    }
+}
